e1af0d2c80302527425b8b70a775fa25a75ae89b
Jiani Cao <jianicakes@gmail.com>
2022-03-07 19:52:21 -0800

    set randomly generated password

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,6 +1,14 @@
 class Employee < ActiveRecord::Base
   belongs_to :store
+
+  before_create :set_password
+
   validates :first_name, :last_name, presence: true
   validates :hourly_rate, numericality: { only_integer: true, greater_than: 40, less_than: 200 }
   validates :store_id, presence: true
+
+  private
+  def set_password
+    self.password = (0...8).map { (65 + rand(26)).chr }.join
+  end
 end
