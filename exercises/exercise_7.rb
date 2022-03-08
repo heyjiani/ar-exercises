f5f0a677a64f02501137a2ba413f79b36cbfb099
Jiani Cao <jianicakes@gmail.com>
2022-03-07 18:14:02 -0800

    create store and display error messages

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,12 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+puts "Enter store name: "
+print "> "
+@input = $stdin.gets.chomp.to_s
+
+error_test = Store.create(
+  name: @input
+)
+
+puts error_test.errors.full_messages
